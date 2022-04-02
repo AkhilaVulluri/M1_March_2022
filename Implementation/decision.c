aeee2f366ea8517f9a91ce24ae4dd5308b80f278
AkhilaVulluri <101192229+AkhilaVulluri@users.noreply.github.com>
2022-04-02 11:54:19 +0530

    Create decision.c

---
+++ Implementation/decision.c
@@ -0,0 +1,25 @@
+int decision()
+{
+    char dec;
+        deci:
+        printf("\n\nPlayer1 %s choose the X or 0:",player1);
+        dec=getchar();
+        scanf("%c",&dec);
+        {
+            if(dec=='X' || dec=='x')
+            {
+                x='X';
+                o='0';
+            }
+            else if(dec=='0')
+            {
+                x='0';
+                o='X';
+            }
+            else
+            {
+                printf("Please enter either X or 0 only \n\n");
+                goto deci;
+            }
+        }
+}
